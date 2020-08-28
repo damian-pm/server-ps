786091c2e05db9a76ec21a2f55b12bae243d0b2c
Damian <damian@portalmorski.pl>
2020-08-28 15:27:19 +0200

    cors

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const app = express()
+var cors = require('cors')
+
 const port = 8000
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+app.get('/upload', (req, res) => {
+  console.log(req,res)
+  res.json({ answer: 42 });
 })
 
 app.listen(port, () => {
