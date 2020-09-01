da4f2381db7ef9f91de289bb6714ec62e5fff5cb
Damian <damian@portalmorski.pl>
2020-09-01 15:05:50 +0200

    upload file

--- index.js
+++ index.js
@@ -2,15 +2,37 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const port = 8000
+var multer = require('multer')
 
 app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// UPLOAD FILE
+var storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+      cb(null, 'public')
+    },
+    filename: function (req, file, cb) {
+      // cb(null, Date.now() + '-' +file.originalname )
+      cb(null, Date.now() + '-' +file.originalname )
+    }
+});
+var upload = multer({ storage: storage }).single('file')
+
 app.post('/upload', (req, res) => {
-  console.log(req,res)
-  res.json({ answer: 42 });
+  // console.log(req,res)
+  upload(req, res, function (err) {
+         if (err instanceof multer.MulterError) {
+             return res.status(500).json(err)
+         } else if (err) {
+             return res.status(500).json(err)
+         }
+      return res.status(200).send(req.file)
+    });
+  // res.json({ answer: 42 });
 })
 
 app.listen(port, () => {
